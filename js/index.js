5431b56c365f9a05164c4cf0d8102bd8810f03d8
Alice <jolynejanssen@icloud.com>
2026-01-21 23:31:30 +0100

    Added dynamic card generation
    This adds both the cards, based on the api and the "advertisements" that come by after a certain amount of cards.

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const container = document.getElementById('cardContainer');
+const images = ["8isk6jv0qy4g1.png", "481708804_18045846239356062_5904598843501618291_n.jpg"];
+const url = 'https://102710.stu.sd-lab.nl/rairaiken/api/gerechten/';
+
+async function getData() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const gerechten = await response.json();
+    console.log(gerechten);
+    let counter = 0;
+    let i = 0;
+
+    for (let gerecht of gerechten) {
+      const cardHTML = `<div class="card" id="override">
+            <img src="./${gerecht.Plaatje}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${gerecht.Naam}</h5>
+                <p class="card-text">${gerecht.Beschrijving}</p>
+                <a href="#" class="btn btn-primary">Bestel</a>
+            </div>
+        </div>`;
+      const imgHTML = `<img src="./img/${images[0+i]}" alt="...">`;
+      counter += 1;
+        console.log(counter);
+
+        if (counter >= 2)
+          {if (i >= images.length){
+            i = 0;
+            container.innerHTML += `${cardHTML}${imgHTML}`
+          } else {
+            counter = 0;
+            i += 1;
+            container.innerHTML += `${cardHTML}${imgHTML}`
+          }
+          
+          
+
+        } else {
+    container.innerHTML += cardHTML; }
+        }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getData(url);
